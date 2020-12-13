789ea14749e0a83584671f13ebe1e8f1bc5d9bd2
cep104 <candicelizabethpeters@gmail.com>
2020-12-13 16:37:11 -0600

    ruby flight problem

---
+++ methods.rb
@@ -0,0 +1,11 @@
+# p 1.next
+# p 10 % 3 
+
+# # parameter
+
+# 'cat'.between?('ant', 'dog') 
+# # returns true
+# 'smart'.between?('ant', 'dog') 
+# # returns false 
+# 3.between?(1, 5) 
+# # returns true
---
+++ movielistflight.rb
@@ -0,0 +1,24 @@
+require 'pry'
+def movie_list(flight, movie_list)
+        i = 0;
+        j = 1;
+        while i <= movie_list.length do
+            if movie_list[i].to_i + movie_list[j].to_i == flight
+                puts true
+                return true  
+            end
+          i += 1
+          j += 1
+          
+       
+      end
+      puts false
+      return false
+
+    
+end
+
+flight = 160 
+movie_list = [60,80,20]
+
+movie_list(flight, movie_list)
